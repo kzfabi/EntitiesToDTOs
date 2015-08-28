71dae623fc741d4722900b3c419688365c0e16ac
Fabian Fernandez Bargas <fabi@kzsoftworks.com>
2015-08-28 01:01:19 -0300

    Removed copyright notice on files. Updated license document.

--- source/EntitiesToDTOs/Helpers/VisualStudioHelper.cs
+++ source/EntitiesToDTOs/Helpers/VisualStudioHelper.cs
@@ -1,11 +1,4 @@
-﻿/* EntitiesToDTOs. Copyright (c) 2011. Fabian Fernandez.
- * http://entitiestodtos.codeplex.com
- * Licensed by Common Development and Distribution License (CDDL).
- * http://entitiestodtos.codeplex.com/license
- * Fabian Fernandez. 
- * http://www.linkedin.com/in/fabianfernandezb/en
- * */
-using EnvDTE;
+﻿using EnvDTE;
 using EnvDTE80;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
